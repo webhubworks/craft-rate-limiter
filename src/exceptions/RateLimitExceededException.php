25334708b879b89052e99c6677b0c9f4d77f8c62
Marven Thieme <thieme@webhub.de>
2025-05-21 14:35:51 +0200

    Create exception class

---
+++ src/exceptions/RateLimitExceededException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace webhubworks\craftratelimiter\exceptions;
+
+use yii\base\Exception;
+
+class RateLimitExceededException extends Exception
+{
+    public function __construct($message = "", $code = 0, \Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
